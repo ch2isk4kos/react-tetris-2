bd47d3e51dd81763c1b123fe40a4de3b12f48d50
ch2isk4kos <ch2isk4kos@gmail.com>
2023-01-28 18:32:01 -0500

    create board component

---
+++ client/src/components/tetris/Board.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Board.css";
+
+const Board = () => {
+  return (
+    <div className="Board">
+      {/*  */}
+      {/*  */}
+      {/*  */}
+    </div>
+  );
+};
+
+export default Board;
