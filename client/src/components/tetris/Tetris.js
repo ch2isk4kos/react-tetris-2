b065c96937980e0729ab89388a45a54629cb2119
ch2isk4kos <ch2isk4kos@gmail.com>
2023-01-28 18:33:43 -0500

    import/render board component

--- client/src/components/tetris/Tetris.js
+++ client/src/components/tetris/Tetris.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Board from "./Board";
 import "../styles/Tetris.css";
 
 const Tetris = ({ rows, columns, setIsGameOver }) => {
   return (
     <div className="Tetris">
       <h1>Tetris</h1>
+      <Board />
       <p>Rows: {rows}</p>
       <p>Columns: {columns}</p>
     </div>
