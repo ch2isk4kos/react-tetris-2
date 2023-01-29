f9948177670ba638a3c7f3b74be8b763e2bf5b5e
ch2isk4kos <ch2isk4kos@gmail.com>
2023-01-28 22:02:50 -0500

    fix: pass object as input to build utility invocation

--- client/src/hooks/useBoard.js
+++ client/src/hooks/useBoard.js
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { buildBoard } from "../utilities/board";
 
 export const useBoard = ({ rows, columns }) => {
-  const [board, setBoard] = useState(buildBoard(rows, columns));
+  const [board] = useState(buildBoard({ rows, columns }));
 
-  return [board, setBoard];
+  return [board];
 };
