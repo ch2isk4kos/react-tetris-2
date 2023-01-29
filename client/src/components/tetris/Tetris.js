4d65c1d8d5d92be2c9f8bf812c4eeae141251d0b
ch2isk4kos <ch2isk4kos@gmail.com>
2023-01-28 20:48:41 -0500

    pass prop(s) to board component

--- client/src/components/tetris/Tetris.js
+++ client/src/components/tetris/Tetris.js
@@ -1,14 +1,13 @@
 import React from "react";
 import Board from "./Board";
+import { useBoard } from "../../hooks/useBoard";
 import "../styles/Tetris.css";
 
 const Tetris = ({ rows, columns, setIsGameOver }) => {
   return (
     <div className="Tetris">
       <h1>Tetris</h1>
-      <Board />
-      <p>Rows: {rows}</p>
-      <p>Columns: {columns}</p>
+      <Board rows={rows} columns={columns} />
     </div>
   );
 };
