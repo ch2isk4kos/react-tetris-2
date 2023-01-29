14174979ba3f42d0aa1f0062df206b4a40878223
ch2isk4kos <ch2isk4kos@gmail.com>
2023-01-28 22:21:17 -0500

    map through matrices to render cells on the board

--- client/src/components/tetris/Board.js
+++ client/src/components/tetris/Board.js
@@ -1,19 +1,22 @@
 import React from "react";
+import Cell from "./Cell";
 import "../styles/Board.css";
 
 const Board = ({ board }) => {
   console.log("board:", board);
 
-  const boardClassStyles = {
+  const classStyles = {
     gridTemplateRows: `repeat(${board.size.rows}, 1fr)`,
     gridTemplateCols: `repeat(${board.size.columns}, 1fr)`,
   };
 
   return (
-    <div className="Board">
-      {/*  */}
-      {/*  */}
-      {/*  */}
+    <div className="Board" style={classStyles}>
+      {board.rows.map((row, y) =>
+        row.map((cell, x) => (
+          <Cell key={x * board.size.columns + x} cell={cell} />
+        ))
+      )}
     </div>
   );
 };
