1f8b7de087e0ded34f5b209faf6b8187953a6b96
ch2isk4kos <ch2isk4kos@gmail.com>
2023-01-28 22:12:05 -0500

    add dynamic style attributes to board component

--- client/src/components/tetris/Board.js
+++ client/src/components/tetris/Board.js
@@ -2,6 +2,13 @@ import React from "react";
 import "../styles/Board.css";
 
 const Board = ({ board }) => {
+  console.log("board:", board);
+
+  const boardClassStyles = {
+    gridTemplateRows: `repeat(${board.size.rows}, 1fr)`,
+    gridTemplateCols: `repeat(${board.size.columns}, 1fr)`,
+  };
+
   return (
     <div className="Board">
       {/*  */}
