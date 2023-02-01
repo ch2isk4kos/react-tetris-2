5189cf08b5d963b6c4737288593ffec317ce9806
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-01 16:26:38 -0500

    add empty array dependency list to track updated state variable(s)

--- client/src/hooks/useStats.js
+++ client/src/hooks/useStats.js
@@ -3,5 +3,5 @@ import { buildStats } from "../utilities/stats";
 
 export const useStats = () => {
   const [stats, setGameStats] = useState(buildStats());
-  const addLinesCleared = useCallback(() => {});
+  const addLinesCleared = useCallback(() => {}, []);
 };
