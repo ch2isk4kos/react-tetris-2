83d3b1e6861ab166b64ad25de7d4050576db9988
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-01 16:35:15 -0500

    change naming convention for game stats children

--- client/src/components/tetris/Stats.js
+++ client/src/components/tetris/Stats.js
@@ -6,7 +6,7 @@ const Stats = ({ stats }) => {
   const lines = linesPerLevel - linesComplete;
 
   return (
-    <ul className="Stats right">
+    <ul className="Stats Stats__right">
       <li>Score</li>
       <li className="value">{level}</li>
       <li>Score</li>
