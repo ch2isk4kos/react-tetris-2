a08b084b289eed433359aa74c3241562c71f4b31
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-01 16:08:06 -0500

    import custom game stats hook && add state variable(s)

--- client/src/components/tetris/Tetris.js
+++ client/src/components/tetris/Tetris.js
@@ -2,10 +2,12 @@ import React from "react";
 import Board from "./Board";
 import Stats from "./Stats";
 import { useBoard } from "../../hooks/useBoard";
+import { useStats } from "../../hooks/useStats";
 import "../styles/Tetris.css";
 
 const Tetris = ({ rows, columns, setIsGameOver }) => {
   const [board, setBoard] = useBoard({ rows, columns });
+  const [stats, setStats] = useStats();
 
   return (
     <div className="Tetris">
