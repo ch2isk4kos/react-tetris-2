e0865c2721172dd51cd82011fe5601c77a561902
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-01 16:20:01 -0500

    move game stats build logic to utilities directory

--- client/src/hooks/useStats.js
+++ client/src/hooks/useStats.js
@@ -1,8 +1,5 @@
 import { useState, useCallback } from "react";
 
-const buildStats = () => ({
-  level: 0,
-  score: 0,
-  linesPerLevel: 10,
-  linesComplete: 0,
-});
+export const useStats = () => {
+  const [stats, setGameStats] = useState();
+};
