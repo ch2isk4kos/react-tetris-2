f63e42d3f90ce5d7700e38038ee98388eaf61a61
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-01 16:22:04 -0500

    import game stats build logic && invoke on state initialization

--- client/src/hooks/useStats.js
+++ client/src/hooks/useStats.js
@@ -1,5 +1,6 @@
 import { useState, useCallback } from "react";
+import { buildStats } from "../utilities/stats";
 
 export const useStats = () => {
-  const [stats, setGameStats] = useState();
+  const [stats, setGameStats] = useState(buildStats());
 };
