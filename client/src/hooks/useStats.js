acc71d8d3e9c923e3ed7adb0128a23ead7c174ae
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-01 16:25:54 -0500

    add callback variable to keep track of lines cleared

--- client/src/hooks/useStats.js
+++ client/src/hooks/useStats.js
@@ -3,4 +3,5 @@ import { buildStats } from "../utilities/stats";
 
 export const useStats = () => {
   const [stats, setGameStats] = useState(buildStats());
+  const addLinesCleared = useCallback(() => {});
 };
