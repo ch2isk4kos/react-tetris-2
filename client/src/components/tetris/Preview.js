d02068e0e0cf68e1903a5d42966104471939a446
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-02 20:54:29 -0500

    add default values to tetrominoes utility initialization

--- client/src/components/tetris/Preview.js
+++ client/src/components/tetris/Preview.js
@@ -7,7 +7,14 @@ const Preview = ({ tetronmino, index }) => {
   const board = buildBoard({ rows: 4, columns: 4 });
   const style = { top: `${index + 15}vw` };
 
-  board.rows = displayTetrominoPreview();
+  board.rows = displayTetrominoPreview({
+    className,
+    isOccupied: false,
+    position: { rows: 0, columns: 0 },
+    rows: board.rows,
+    shape,
+  });
+
   return (
     <div className="Preview">
       {/*  */}
