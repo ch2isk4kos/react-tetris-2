fb2580e30a65d8bdc53a1c4e77272104dc096dbc
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-02 20:07:14 -0500

    create file for preview component

---
+++ client/src/components/tetris/Preview.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Preview = ({ tetrominos }) => {
+  return (
+    <div className="Preview">
+      {/*  */}
+      {/*  */}
+    </div>
+  );
+};
+
+export default Preview;
