95ce103bded93961eb7f6e120cdfd64fb74eae02
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-02 20:48:42 -0500

    create variable for dynamic inline styling

--- client/src/components/tetris/Preview.js
+++ client/src/components/tetris/Preview.js
@@ -4,6 +4,7 @@ import { buildBoard } from "../../utilities/board";
 const Preview = ({ tetronmino, index }) => {
   const { shape, className } = tetronmino;
   const board = buildBoard({ rows: 4, columns: 4 });
+  const style = { top: `${index + 15}vw` };
   return (
     <div className="Preview">
       {/*  */}
