6fe9fce94b461f89b839f3a2ef2baa4a40b243c6
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-02 20:30:13 -0500

    import board utility and define variable to initialize

--- client/src/components/tetris/Preview.js
+++ client/src/components/tetris/Preview.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { buildBoard } from "../../utilities/board";
 
 const Preview = ({ tetronmino, index }) => {
+  const board = buildBoard({ rows: 4, columns: 4 });
   return (
     <div>
       {/*  */}
