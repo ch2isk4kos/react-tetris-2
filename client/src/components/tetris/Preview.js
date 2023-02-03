1cfaf733464644c006942396699a35789de5df42
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-02 20:13:41 -0500

    create variable to store each tetromino minus last

--- client/src/components/tetris/Preview.js
+++ client/src/components/tetris/Preview.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Preview = ({ tetrominoes }) => {
+  const preview = tetrominoes.slice(1 - tetrominoes.length).reverse();
   return (
     <div className="Preview">
       {/*  */}
