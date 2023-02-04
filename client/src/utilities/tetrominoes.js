aadfbbfe3870d39d3d1433619c5dc4fc23414c7a
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-04 18:26:11 -0500

    return the random property from the tetrominoes global object

--- client/src/utilities/tetrominoes.js
+++ client/src/utilities/tetrominoes.js
@@ -63,6 +63,7 @@ export const randomTetromino = () => {
   const keys = Object.keys(TETROMINOES);
   const index = Math.floor(Math.random() * keys.length);
   const key = keys[index];
+  return TETROMINOES[key];
 };
 
 export const displayTetromino = ({
