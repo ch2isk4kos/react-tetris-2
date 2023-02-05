1e9244b62fbd66ebbc4c0d81bb7665793971087f
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-05 14:24:52 -0500

    add boilerplate for expected return values

--- client/src/utilities/player.js
+++ client/src/utilities/player.js
@@ -1,1 +1,11 @@
-export const buildPlayer = (previous) => {};
+export const buildPlayer = (previous) => {
+  let tetrominoes;
+
+  return {
+    collided: false,
+    isFastDropping: false,
+    position: { row: 0, column: 4 },
+    tetrominoes,
+    tetromino: tetrominoes.pop(),
+  };
+};
