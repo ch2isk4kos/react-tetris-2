e8cd979cb0965b8ae88247d36112b8e2942f7c45
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-05 15:40:17 -0500

    add className for previews parent

--- client/src/components/tetris/Previews.js
+++ client/src/components/tetris/Previews.js
@@ -1,14 +1,15 @@
 import React from "react";
 import Preview from "./Preview";
+import "../styles/Previews.css";
 
 const Previews = ({ tetrominoes }) => {
   const previews = tetrominoes.slice(1 - tetrominoes.length).reverse();
   return (
-    <>
+    <div className="Previews">
       {previews.map((tetromino, index) => (
         <Preview tetromino={tetromino} index={index} key={index} />
       ))}
-    </>
+    </div>
   );
 };
 
