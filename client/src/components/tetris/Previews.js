e00b0831ab1b69e96b11fb0b95ce2709cca762f9
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-05 15:24:20 -0500

    change render of div element to fragment(s)

--- client/src/components/tetris/Previews.js
+++ client/src/components/tetris/Previews.js
@@ -4,11 +4,11 @@ import Preview from "./Preview";
 const Previews = ({ tetrominoes }) => {
   const previews = tetrominoes.slice(1 - tetrominoes.length).reverse();
   return (
-    <div className="Previews">
+    <>
       {previews.map((tetromino, index) => (
         <Preview tetromino={tetromino} index={index} key={index} />
       ))}
-    </div>
+    </>
   );
 };
 
