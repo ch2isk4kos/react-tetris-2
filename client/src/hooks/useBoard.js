84847642dd3d9b963d687e32fae08caf512c3025
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-06 21:48:09 -0500

    add additional parameter(s) to useBoard custom hook

--- client/src/hooks/useBoard.js
+++ client/src/hooks/useBoard.js
@@ -1,7 +1,13 @@
 import { useState } from "react";
 import { buildBoard } from "../utilities/board";
 
-export const useBoard = ({ rows, columns }) => {
+export const useBoard = ({
+  rows,
+  columns,
+  player,
+  resetPlayer,
+  addLinesCleared,
+}) => {
   const [board] = useState(buildBoard({ rows, columns }));
 
   return [board];
