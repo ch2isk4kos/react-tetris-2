fd3d8ea65b6e51c22d96049e99d4cd99add8925f
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-07 12:49:23 -0500

    remove import

--- client/src/hooks/usePlayer.js
+++ client/src/hooks/usePlayer.js
@@ -1,7 +1,6 @@
 import { useState, useCallback } from "react";
 
 import { buildPlayer } from "../utilities/player";
-import { randomTetromino } from "../utilities/tetrominoes";
 
 export const usePlayer = () => {
   const [player, setPlayer] = useState(buildPlayer());
