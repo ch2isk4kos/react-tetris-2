caa261e1497769c1049542f263fbdfaa48c97404
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-06 22:04:08 -0500

    import board utility function

--- client/src/hooks/useBoard.js
+++ client/src/hooks/useBoard.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { buildBoard } from "../utilities/board";
+import { buildBoard, nextBoard } from "../utilities/board";
 
 export const useBoard = ({
   rows,
