f0836c0a6f6622545356d6f96fae1abc2b972871
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-10 12:53:33 -0500

    create global object of key presses

---
+++ client/src/utilities/input/action.js
@@ -0,0 +1,9 @@
+export const Key = {
+  ArrowUp: Action.Rotate,
+  ArrowRight: Action.Right,
+  ArrowDown: Action.SlowDrop,
+  ArrowLeft: Action.Left,
+  KeyQ: Action.Quit,
+  KeyP: Action.Pause,
+  Space: Action.FastDrop,
+};
