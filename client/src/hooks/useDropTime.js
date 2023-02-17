f492ba2caefcaadb418088ac1f32be786e68c837
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-17 12:42:10 -0500

    import useEffect native hook

--- client/src/hooks/useDropTime.js
+++ client/src/hooks/useDropTime.js
@@ -1,4 +1,4 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 
 export const useDropTime = ({ stats }) => {
   const [dropTime, setDropTime] = useState(0);
