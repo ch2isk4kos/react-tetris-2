7f652004cac87aa8b947bbd66276fd7e14012dc7
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-17 12:42:31 -0500

    declare global variable(s)

--- client/src/hooks/useDropTime.js
+++ client/src/hooks/useDropTime.js
@@ -1,5 +1,9 @@
 import { useState, useCallback, useEffect } from "react";
 
+const DefaultDropTime = 1000;
+const MinimumDropTime = 100;
+const Speed = 50;
+
 export const useDropTime = ({ stats }) => {
   const [dropTime, setDropTime] = useState(0);
   return [dropTime, pauseDropTime, resumeDropTime];
