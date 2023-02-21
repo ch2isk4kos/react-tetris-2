a79a8d61c6e1f5d2cf77c2ce5d6602e530c63b75
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-21 12:59:00 -0500

    addLinesCleared: change naming convention of variable(s)

--- client/src/hooks/useStats.js
+++ client/src/hooks/useStats.js
@@ -6,12 +6,12 @@ export const useStats = () => {
   const addLinesCleared = useCallback((lines) => {
     // update game stats anytime lines have cleared
     setGameStats((previous) => {
-      const points = previous.points + lines + 100;
+      const score = previous.points + lines + 100;
       const { linesPerLevel } = previous;
-      const linesCompleted = previous.linesComplete + lines;
+      const completedLines = previous.linesComplete + lines;
       const level =
-        linesCompleted >= linesPerLevel ? previous.level + 1 : previous.level;
-      const completedLines = linesCompleted % linesPerLevel;
+        completedLines >= linesPerLevel ? previous.level + 1 : previous.level;
+      const linesComplete = completedLines % linesPerLevel;
       // output updated game stats
     }, []);
   }, []);
