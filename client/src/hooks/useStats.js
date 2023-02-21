dcefc1634725d7dcd81c20739b5d596ab2889508
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-21 12:42:07 -0500

    addLinesCleared: invoke setter to update game stats

--- client/src/hooks/useStats.js
+++ client/src/hooks/useStats.js
@@ -5,6 +5,7 @@ export const useStats = () => {
   const [stats, setGameStats] = useState(buildStats());
   const addLinesCleared = useCallback((lines) => {
     // update game stats anytime lines have cleared
+    setGameStats((previous) => {}, []);
   }, []);
 
   return [stats, addLinesCleared];
