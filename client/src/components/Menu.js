03a26e2334434d53cc0d61cba97d42934b6bb7a9
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-23 14:45:13 -0500

    test: remove background image

--- client/src/components/Menu.js
+++ client/src/components/Menu.js
@@ -5,7 +5,7 @@ const Menu = ({ start }) => {
   return (
     <div className="Menu">
       <div className="column col-1">
-        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.ao7ZvoE2IbP1pTz4xRXq_QHaEK%26pid%3DApi&f=1&ipt=45b65e059dc575f5c35139ab02e0620dd19f21d251be221b21dfe0309cd61d91&ipo=images"></img>
+        <img src=""></img>
       </div>
       <div className="column col-2">
         <div className="play-button">
