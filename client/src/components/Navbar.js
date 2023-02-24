70706f65e3d1d1f78553addc9c02f937abe4ebc9
ch2isk4kos <ch2isk4kos@gmail.com>
2023-02-24 13:55:26 -0500

    wrap list item(s) around anchor tag(s)

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,18 @@
 const Navbar = () => {
   return (
     <ul className="Navbar">
-      <li>Logo</li>
-      <li>Something</li>
-      <li>About</li>
-      <li>Contact</li>
+      <li>
+        <a href="">Logo</a>
+      </li>
+      <li>
+        <a href="">Something</a>
+      </li>
+      <li>
+        <a href="">About</a>
+      </li>
+      <li>
+        <a href="">Contact</a>
+      </li>
     </ul>
   );
 };
