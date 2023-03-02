b19afb490905ba2e9d63ecafc4d154d29bf4ec37
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-02 13:18:34 -0500

    structure right side nav item(s) into a container div

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,27 +6,29 @@ const Navbar = () => {
       <li id="logo" className="nav-item">
         <p>Logo</p>
       </li>
-      <li className="nav-item">
-        <a
-          href="https://github.com/ch2isk4kos"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Something
-        </a>
-      </li>
-      <li className="nav-item">
-        <a href="#">About</a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="https://twitter.com/ch2isk4kos"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Contact
-        </a>
-      </li>
+      <div className="nav-links">
+        <li className="nav-item">
+          <a
+            href="https://github.com/ch2isk4kos"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Something
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="#">About</a>
+        </li>
+        <li className="nav-item">
+          <a
+            href="https://twitter.com/ch2isk4kos"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Contact
+          </a>
+        </li>
+      </div>
     </ul>
   );
 };
