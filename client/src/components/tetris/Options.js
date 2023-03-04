c86b30f3441d12e6e5a613b0d9ea05c7094305f5
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-04 15:16:20 -0500

    create component for game options

---
+++ client/src/components/tetris/Options.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Options = () => {
+  return (
+    <ul className="Options">
+      <li>
+        <h2>Options</h2>
+      </li>
+      <li className="keypress">P</li>
+      <li className="keypress">Q</li>
+      <li className="keypress">Up Arrow</li>
+      <li className="keypress">Down Arrow</li>
+      <li className="keypress">Spacebar</li>
+    </ul>
+  );
+};
+
+export default Options;
