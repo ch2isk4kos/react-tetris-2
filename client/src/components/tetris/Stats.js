d839cb467f17f17bef2eb8a0989910591583843e
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-04 15:14:20 -0500

    add game stats to left side of window

--- client/src/components/tetris/Stats.js
+++ client/src/components/tetris/Stats.js
@@ -6,20 +6,22 @@ const Stats = ({ stats }) => {
   const lines = linesPerLevel - linesComplete;
 
   return (
-    <ul className="Stats Stats__right">
-      <li>
-        <h2>Level</h2>
-      </li>
-      <li className="value">{level}</li>
-      <li>
-        <h2>Lines</h2>
-      </li>
-      <li className="value">{lines}</li>
-      <li>
-        <h2>Score</h2>
-      </li>
-      <li className="value">{score}</li>
-    </ul>
+    <>
+      <ul className="Stats Stats__left">
+        <li>
+          <h2>Level</h2>
+        </li>
+        <li className="value">{level}</li>
+        <li>
+          <h2>Lines</h2>
+        </li>
+        <li className="value">{lines}</li>
+        <li>
+          <h2>Score</h2>
+        </li>
+        <li className="value">{score}</li>
+      </ul>
+    </>
   );
 };
 
