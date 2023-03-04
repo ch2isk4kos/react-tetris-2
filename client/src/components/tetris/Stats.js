309434ea4ff3260f8348d8cddca4ed2ca384b36b
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-04 15:23:26 -0500

    remove game options code && reassign class names

--- client/src/components/tetris/Stats.js
+++ client/src/components/tetris/Stats.js
@@ -6,32 +6,20 @@ const Stats = ({ stats }) => {
   const lines = linesPerLevel - linesComplete;
 
   return (
-    <>
-      <ul className="Stats Stats__left">
-        <li>
-          <h2>Level</h2>
-        </li>
-        <li className="value">{level}</li>
-        <li>
-          <h2>Lines</h2>
-        </li>
-        <li className="value">{lines}</li>
-        <li>
-          <h2>Score</h2>
-        </li>
-        <li className="value">{score}</li>
-      </ul>
-      {/* <ul className="Stats Stats__right">
-        <li>
-          <h2>Options</h2>
-        </li>
-        <li className="keypress">P</li>
-        <li className="keypress">Q</li>
-        <li className="keypress">Up Arrow</li>
-        <li className="keypress">Down Arrow</li>
-        <li className="keypress">Spacebar</li>
-      </ul> */}
-    </>
+    <ul className="Stats">
+      <li>
+        <h2>Level</h2>
+      </li>
+      <li className="value">{level}</li>
+      <li>
+        <h2>Lines</h2>
+      </li>
+      <li className="value">{lines}</li>
+      <li>
+        <h2>Score</h2>
+      </li>
+      <li className="value">{score}</li>
+    </ul>
   );
 };
 
