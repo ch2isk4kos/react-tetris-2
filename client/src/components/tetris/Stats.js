d1bee802d202890f8a6eee7021b954d61f42c1d0
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-04 15:16:59 -0500

    comment code

--- client/src/components/tetris/Stats.js
+++ client/src/components/tetris/Stats.js
@@ -21,6 +21,16 @@ const Stats = ({ stats }) => {
         </li>
         <li className="value">{score}</li>
       </ul>
+      {/* <ul className="Stats Stats__right">
+        <li>
+          <h2>Options</h2>
+        </li>
+        <li className="keypress">P</li>
+        <li className="keypress">Q</li>
+        <li className="keypress">Up Arrow</li>
+        <li className="keypress">Down Arrow</li>
+        <li className="keypress">Spacebar</li>
+      </ul> */}
     </>
   );
 };
