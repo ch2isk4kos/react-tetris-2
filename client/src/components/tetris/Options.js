3683e58c87641538f6c2e690c489fb9b792e2408
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-06 13:44:57 -0500

    remove class name(s)

--- client/src/components/tetris/Options.js
+++ client/src/components/tetris/Options.js
@@ -4,23 +4,23 @@ import "../styles/Options.css";
 const Options = () => {
   return (
     <ul className="Options">
-      <li className="option">
+      <li>
         <span className="keypress">P</span>{" "}
         <span className="keypress-action">[ :pause :resume ]</span>
       </li>
-      <li className="option">
+      <li>
         <span className="keypress">Q</span>{" "}
         <span className="keypress-action">:quit</span>
       </li>
-      <li className="option">
+      <li>
         <span className="keypress">Up Arrow</span>{" "}
         <span className="keypress-action">:rotate</span>
       </li>
-      <li className="option">
+      <li>
         <span className="keypress">Down Arrow</span>{" "}
         <span className="keypress-action">:slow drop</span>
       </li>
-      <li className="option">
+      <li>
         <span className="keypress">Spacebar</span>{" "}
         <span className="keypress-action">:fast drop</span>
       </li>
