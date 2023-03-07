c34fa1f28d29d7b60b47283d23fe2b35cae5ea27
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-07 14:27:43 -0500

    wrap row(s) in a container div

--- client/src/components/navbar/Sidebar.js
+++ client/src/components/navbar/Sidebar.js
@@ -4,11 +4,13 @@ const Sidebar = () => {
   return (
     <div className="Sidebar">
       <h1>Sidebar</h1>
-      <div className="row-1"></div>
-      <div className="row-2"></div>
-      <div className="row-3"></div>
-      <div className="row-4"></div>
-      <div className="row-5"></div>
+      <div className="rows">
+        <div className="row-1"></div>
+        <div className="row-2"></div>
+        <div className="row-3"></div>
+        <div className="row-4"></div>
+        <div className="row-5"></div>
+      </div>
     </div>
   );
 };
