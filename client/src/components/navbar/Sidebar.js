c642371b195aca1d04801d3fb3a3a0b2baeeb69e
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-07 14:26:57 -0500

    add row(s) to off-canvas

--- client/src/components/navbar/Sidebar.js
+++ client/src/components/navbar/Sidebar.js
@@ -4,6 +4,11 @@ const Sidebar = () => {
   return (
     <div className="Sidebar">
       <h1>Sidebar</h1>
+      <div className="row-1"></div>
+      <div className="row-2"></div>
+      <div className="row-3"></div>
+      <div className="row-4"></div>
+      <div className="row-5"></div>
     </div>
   );
 };
