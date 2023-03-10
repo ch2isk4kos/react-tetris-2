d883fda6378df25ba299235949a8dcd25c3cbfe9
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-10 13:35:56 -0500

    remove bloat code

--- client/src/components/Landing.js
+++ client/src/components/Landing.js
@@ -1,12 +1,16 @@
 import React from "react";
+import logo from "../assets/images/logo.svg";
 import "./styles/Landing.css";
 
 const Landing = ({ start }) => {
   return (
     <div className="Landing">
-      <button className="button" onClick={start}>
-        Play
-      </button>
+      <header className="landing-header">
+        <img src={logo} className="react-logo" alt="logo" />
+        <button className="button" onClick={start}>
+          Play
+        </button>
+      </header>
     </div>
   );
 };
