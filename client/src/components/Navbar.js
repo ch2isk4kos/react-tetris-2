fb41aa156b7e15eaf2eff29ef71bcba607a20ae7
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-11 16:20:24 -0500

    remove import(s) not in use

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,7 +1,4 @@
-import gitHub from "../assets/images/icons/github_icon.png";
 import hamburger from "../assets/images/icons/hamburger_icon.png";
-import medium from "../assets/images/icons/medium_icon.png";
-import twitter from "../assets/images/icons/twitter_icon.png";
 import "./styles/Navbar.css";
 
 const Navbar = () => {
