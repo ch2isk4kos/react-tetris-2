0fb26a1a9bd2c1fca3a515ba7f71422da787a625
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-13 14:36:17 -0400

    add class name to list item(s)

--- client/src/components/tetris/Options.js
+++ client/src/components/tetris/Options.js
@@ -4,23 +4,23 @@ import "../styles/Options.css";
 const Options = () => {
   return (
     <ul className="Options">
-      <li>
+      <li className="action">
         <span className="keypress">P</span>{" "}
         <span className="keypress-action">:pause :resume</span>
       </li>
-      <li>
+      <li className="action">
         <span className="keypress">Q</span>{" "}
         <span className="keypress-action">:quit</span>
       </li>
-      <li>
+      <li className="action">
         <span className="keypress">Up Arrow</span>{" "}
         <span className="keypress-action">:rotate</span>
       </li>
-      <li>
+      <li className="action">
         <span className="keypress">Down Arrow</span>{" "}
         <span className="keypress-action">:slow drop</span>
       </li>
-      <li>
+      <li className="action">
         <span className="keypress">Spacebar</span>{" "}
         <span className="keypress-action">:fast drop</span>
       </li>
