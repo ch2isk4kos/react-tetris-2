7ada756e9fae2c276066514861274b61c1de30f4
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-15 14:17:34 -0400

    add conditional statement to open/close hamburger on click

--- client/src/components/navigation/Navbar.js
+++ client/src/components/navigation/Navbar.js
@@ -6,19 +6,65 @@ import {
   faEnvelope,
   faHippo,
   faUserTie,
+  faXmark,
 } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import "../styles/Navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOnOpen = () => {
+    setIsOpen(true);
+  };
+
+  const handleOnClose = (e) => {
+    e.preventDefault();
+    setIsOpen(false);
+    console.log("event:", e);
+    console.log("event:", e.target.viewPortElement);
+  };
+
   return (
     <ul className="Navbar">
       <li id="logo" className="nav-item">
         <p>Logo</p>
       </li>
-      <li id="hamburger" className="nav-item">
+      {/* <li id="hamburger" className="nav-item" onClick={handleOnOpen}>
         <FontAwesomeIcon icon={faBars} size="2x" />
-      </li>
+      </li> */}
+      {isOpen ? (
+        <div className="hamburger-icons">
+          <li className="hamburger-icon">
+            <FontAwesomeIcon
+              className="close-icon"
+              icon={faXmark}
+              size="2x"
+              onClick={handleOnClose}
+            />
+          </li>
+          <li className="hamburger-icon">
+            <FontAwesomeIcon icon={faUserTie} size="2x" />
+          </li>
+          <li className="hamburger-icon">
+            <FontAwesomeIcon icon={faComments} size="2x" />
+          </li>
+          <li className="hamburger-icon">
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          </li>
+          <li className="hamburger-icon">
+            <FontAwesomeIcon icon={faHippo} size="2x" />
+          </li>
+        </div>
+      ) : (
+        <li
+          id="hamburger"
+          className="nav-item"
+          onClick={(e) => handleOnOpen(e)}
+        >
+          <FontAwesomeIcon icon={faBars} size="2x" />
+        </li>
+      )}
     </ul>
   );
 };
