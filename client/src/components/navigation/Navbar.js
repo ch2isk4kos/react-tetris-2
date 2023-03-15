06a2f0994cdb6954c57aea3d051eff331a706cd8
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-15 14:43:53 -0400

    remove conditional render of hamburger item(s)

--- client/src/components/navigation/Navbar.js
+++ client/src/components/navigation/Navbar.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBars,
@@ -7,60 +6,17 @@ import {
   faUserTie,
   faXmark,
 } from "@fortawesome/free-solid-svg-icons";
-import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import "../styles/Navbar.css";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleOnOpen = () => {
-    setIsOpen(true);
-  };
-
-  const handleOnClose = (e) => {
-    e.preventDefault();
-    setIsOpen(false);
-    console.log("event:", e);
-    console.log("event:", e.target.viewPortElement);
-  };
-
   return (
     <ul className="Navbar">
-      <li id="logo" className="nav-item">
+      <li id="logo">
         <p>Logo</p>
       </li>
-      {isOpen ? (
-        <div className="hamburger-icons">
-          <li className="hamburger-icon">
-            <FontAwesomeIcon
-              className="close-icon"
-              icon={faXmark}
-              size="2x"
-              onClick={handleOnClose}
-            />
-          </li>
-          <li className="hamburger-icon">
-            <FontAwesomeIcon icon={faUserTie} size="2x" />
-          </li>
-          <li className="hamburger-icon">
-            <FontAwesomeIcon icon={faComments} size="2x" />
-          </li>
-          <li className="hamburger-icon">
-            <FontAwesomeIcon icon={faLinkedin} size="2x" />
-          </li>
-          <li className="hamburger-icon">
-            <FontAwesomeIcon icon={faHippo} size="2x" />
-          </li>
-        </div>
-      ) : (
-        <li
-          id="hamburger"
-          className="nav-item"
-          onClick={(e) => handleOnOpen(e)}
-        >
-          <FontAwesomeIcon icon={faBars} size="2x" />
-        </li>
-      )}
+      <li id="hamburger">
+        <FontAwesomeIcon icon={faBars} size="2x" />
+      </li>
     </ul>
   );
 };
