080dbe0751d79355c98e019c8412bfc8d9da80e3
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-15 11:59:05 -0400

    import useState hook

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import "./styles/Navbar.css";
