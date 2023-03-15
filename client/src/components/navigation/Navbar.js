b66bc7a9513e3656b88d9c8c587da90aa257894c
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-15 13:28:18 -0400

    remove floating action button dependencies

--- client/src/components/navigation/Navbar.js
+++ client/src/components/navigation/Navbar.js
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import FloatingActionButton from "./FloatingActionButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBars,
@@ -12,36 +11,13 @@ import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import "../styles/Navbar.css";
 
 const Navbar = () => {
-  const actions = [
-    {
-      label: "About",
-      icon: <FontAwesomeIcon icon={faUserTie} size="2x" />,
-      onClick: console.log,
-    },
-    {
-      label: "Contact",
-      icon: <FontAwesomeIcon icon={faComments} size="2x" />,
-      onClick: console.log,
-    },
-    {
-      label: "Me",
-      icon: <FontAwesomeIcon icon={faHippo} size="2x" />,
-      onClick: console.log,
-    },
-    {
-      label: "Connect",
-      icon: <FontAwesomeIcon icon={faLinkedin} size="2x" />,
-      onClick: console.log,
-    },
-  ];
   return (
     <ul className="Navbar">
       <li id="logo" className="nav-item">
         <p>Logo</p>
       </li>
       <li id="hamburger" className="nav-item">
-        {/* <FontAwesomeIcon icon={faBars} size="2x" /> */}
-        <FloatingActionButton actions={actions} />
+        <FontAwesomeIcon icon={faBars} size="2x" />
       </li>
     </ul>
   );
