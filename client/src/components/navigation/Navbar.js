6a0dada325e70091a1c60fcf0b8fb774f90c9ed4
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-15 15:00:37 -0400

    test: add icon(s) to navbar

--- client/src/components/navigation/Navbar.js
+++ client/src/components/navigation/Navbar.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faBars,
@@ -6,9 +7,23 @@ import {
   faUserTie,
   faXmark,
 } from "@fortawesome/free-solid-svg-icons";
+import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import "../styles/Navbar.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOnOpen = () => {
+    setIsOpen(true);
+  };
+
+  const handleOnClose = (e) => {
+    e.preventDefault();
+    setIsOpen(false);
+    console.log("event:", e);
+    console.log("event:", e.target.viewPortElement);
+  };
+
   return (
     <ul className="Navbar">
       <li id="logo">
@@ -17,6 +32,28 @@ const Navbar = () => {
       <li id="hamburger">
         <FontAwesomeIcon icon={faBars} size="2x" />
       </li>
+      <div className="icons">
+        <li className="hamburger-icon">
+          <a href="https://google.com" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faUserTie} size="2x" />
+          </a>
+        </li>
+        <li className="hamburger-icon">
+          <a href="https://google.com" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faComments} size="2x" />
+          </a>
+        </li>
+        <li className="hamburger-icon">
+          <a href="https://google.com" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} size="2x" />
+          </a>
+        </li>
+        <li className="hamburger-icon">
+          <a href="https://google.com" target="_blank" rel="noreferrer">
+            <FontAwesomeIcon icon={faHippo} size="2x" />
+          </a>
+        </li>
+      </div>
     </ul>
   );
 };
