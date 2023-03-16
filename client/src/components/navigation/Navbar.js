cf4fa26a3f5ee8851125286817bfdde21693a54f
ch2isk4kos <ch2isk4kos@gmail.com>
2023-03-16 13:17:24 -0400

    remove additional menu item(s)

--- client/src/components/navigation/Navbar.js
+++ client/src/components/navigation/Navbar.js
@@ -11,19 +11,6 @@ import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import "../styles/Navbar.css";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const handleOnOpen = () => {
-    setIsOpen(true);
-  };
-
-  const handleOnClose = (e) => {
-    e.preventDefault();
-    setIsOpen(false);
-    console.log("event:", e);
-    console.log("event:", e.target.viewPortElement);
-  };
-
   return (
     <ul className="Navbar">
       <li id="logo">
@@ -32,28 +19,6 @@ const Navbar = () => {
       <li id="hamburger">
         <FontAwesomeIcon icon={faBars} size="2x" />
       </li>
-      <div className="icons">
-        <li className="hamburger-icon">
-          <a href="https://google.com" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faUserTie} size="2x" />
-          </a>
-        </li>
-        <li className="hamburger-icon">
-          <a href="https://google.com" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faComments} size="2x" />
-          </a>
-        </li>
-        <li className="hamburger-icon">
-          <a href="https://google.com" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faLinkedin} size="2x" />
-          </a>
-        </li>
-        <li className="hamburger-icon">
-          <a href="https://google.com" target="_blank" rel="noreferrer">
-            <FontAwesomeIcon icon={faHippo} size="2x" />
-          </a>
-        </li>
-      </div>
     </ul>
   );
 };
